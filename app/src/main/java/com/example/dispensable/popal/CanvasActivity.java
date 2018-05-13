d444b3bde59cb0935b540cb26aa189925d3a4bf1
wangqiang <wangqiang@wanwujieshu.net>
2018-05-13 22:49:26 +0800

    [feature] firefly fly

--- app/src/main/java/com/example/dispensable/popal/CanvasActivity.java
+++ app/src/main/java/com/example/dispensable/popal/CanvasActivity.java
@@ -1,19 +1,37 @@
 package com.example.dispensable.popal;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.app.Activity;
+import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieImageAsset;
+
+import java.lang.reflect.Method;
 
 
 public class CanvasActivity extends BlunoLibrary {
     private boolean hasConnected = false;
     private AudioRecordDemo audioRecordDemo;
     private static Handler mainHandler;
+    private LottieAnimationView lottieAnimationView;
+    private boolean isInAnimationCycle = false;
+
+    public enum fireflyStatus {
+        still,
+        out,
+        in
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +46,36 @@ public class CanvasActivity extends BlunoLibrary {
         {
             public void dispatchMessage(android.os.Message msg) {
                 Toast.makeText(CanvasActivity.this, "Get: " + msg.what, Toast.LENGTH_LONG).show();
+                if (isInAnimationCycle) {
+                    return;
+                }
+
                 if (msg.what == 1) {
                     Log.e("VVVVVVVV", "1");
+                    isInAnimationCycle = true;
+                    showFirefly(fireflyStatus.out, 0);
                     serialSend("1");
+                    showFirefly(fireflyStatus.still, 15);
+                    showFirefly(fireflyStatus.in, 1);
+                    showFirefly(fireflyStatus.still, 1);
+                    isInAnimationCycle = false;
                 } else if (msg.what == 2) {
                     Log.e("VVVVVVVV", "2");
                     serialSend("2");
                 }
             }
         };
+
+        setFullScreen(this);
+        if (hasNavBar(this)) {
+            hideBottomUIMenu();
+        }
+
+        setScreenOn();
+
+        // set firefly still
+        lottieAnimationView = (LottieAnimationView) findViewById(R.id.animation_view);
+        showFirefly(fireflyStatus.out, 1);
     }
 
     @Override
@@ -114,6 +153,69 @@ public class CanvasActivity extends BlunoLibrary {
         audioRecordDemo.getNoiseLevel();
     }
 
+    /**
+     * 隐藏虚拟按键，并且全屏
+     */
+    protected void hideBottomUIMenu(){
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE;
+            decorView.setSystemUiVisibility(uiOptions);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 检查是否存在虚拟按键栏
+     * @param context
+     * @return
+     */
+    public static boolean hasNavBar(Context context) {
+        Resources res = context.getResources();
+        int resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (resourceId != 0) {
+            boolean hasNav = res.getBoolean(resourceId);
+            // check override flag
+            String sNavBarOverride = getNavBarOverride();
+            if ("1".equals(sNavBarOverride)) {
+                hasNav = false;
+            } else if ("0".equals(sNavBarOverride)) {
+                hasNav = true;
+            }
+            return hasNav;
+        } else { // fallback
+            return !ViewConfiguration.get(context).hasPermanentMenuKey();
+        }
+    }
+
+    /**
+     * 判断虚拟按键栏是否重写
+     * @return
+     */
+    private static String getNavBarOverride() {
+        String sNavBarOverride = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                Class c = Class.forName("android.os.SystemProperties");
+                Method m = c.getDeclaredMethod("get", String.class);
+                m.setAccessible(true);
+                sNavBarOverride = (String) m.invoke(null, "qemu.hw.mainkeys");
+            } catch (Throwable e) {
+            }
+        }
+        return sNavBarOverride;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         onActivityResultProcess(requestCode, resultCode, data);					//onActivityResult Process by BlunoLibrary
@@ -141,6 +243,30 @@ public class CanvasActivity extends BlunoLibrary {
     protected void onDestroy() {
         super.onDestroy();
         onDestroyProcess();														//onDestroy Process by BlunoLibrary
+    }
+
+    private void setScreenOn() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    private void showAnimation(String imageFolder, String jsonFile, int repeatCount) {
+        lottieAnimationView.setRepeatCount(repeatCount);
+        lottieAnimationView.setImageAssetsFolder(imageFolder);
+        lottieAnimationView.setAnimation(jsonFile);
+        lottieAnimationView.playAnimation();
+    }
+
+    private void showFirefly(fireflyStatus status, int repeatCount) {
+        switch (status) {
+            case still:
+                showAnimation("firefly_still_image", "firefly_still_data", repeatCount);
+                break;
+            case in:
+                showAnimation("firefly_in_image", "firefly_in_data", repeatCount);
+                break;
+            case out:
+                showAnimation("firefly_out_image", "firefly_out_data", repeatCount);
+        }
     }
 
 }
